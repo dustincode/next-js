93b5f20b682608bdaaf1e8c72d74a7cec24a086c
datnt <datnt@sparkminds.net>
2022-01-07 05:33:06 +0700

    Add supabase

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,14 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+);
 
 type Data = {
-  name: string;
+  status: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  console.log('Headers:', req.headers);
-  console.log('Cookie:', req.cookies);
-  console.log('Query: ', req.query);
-  console.log('Body:', req.body);
-  res.status(204).json({ name: 'Dustin Ng' });
+const storeEventObject = async (req: NextApiRequest) => {
+  const { headers, cookies, query, body } = req;
+  const { error } = await supabase.from('fireblocks_webhook').insert([
+    {
+      headers,
+      cookie: cookies,
+      query,
+      body,
+    },
+  ]);
+  console.log('Error: ', error);
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  await storeEventObject(req);
+  res.status(204).json({ status: 'OK' });
 }
